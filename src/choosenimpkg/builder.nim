b0a2a3466b3386cb2a71fc8fb8d68ab380ee7eb1
Jake Leahy <jake@leahy.dev>
2024-04-27 10:22:28 +1000

    Change how we make choosenim always build for arm
    Now just in the build section we check if we are arm
    
    This way we are not changing the names

--- src/choosenimpkg/builder.nim
+++ src/choosenimpkg/builder.nim
@@ -1,4 +1,4 @@
-import os, times
+import os, times, strutils
 
 import nimblepkg/[version, cli]
 import nimblepkg/common as nimble_common
@@ -126,7 +126,7 @@ proc build*(extractDir: string, version: Version, params: CliParams) =
 
   var success = false
   try:
-    if fileExists(getCurrentDir() / "bin" / "nim".addFileExt(ExeExt)):
+    if fileExists(getCurrentDir() / "bin" / "nim".addFileExt(ExeExt)) and not getArch().startsWith("arm"):
       if not version.isDevel() or not params.latest:
         display("Compiler:", "Already built", priority = HighPriority)
         success = true
